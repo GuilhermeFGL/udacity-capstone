ccfc2c81b11c6f16f49dd2f5805330403638f69c
guilhermefgl <Leonardo.souza@Montreal.com.br>
2018-05-11 16:21:31 -0300

    crud trip layout

--- app/src/main/java/com/guilhermefgl/rolling/view/BaseActivity.java
+++ app/src/main/java/com/guilhermefgl/rolling/view/BaseActivity.java
@@ -1,7 +1,12 @@
 package com.guilhermefgl.rolling.view;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
 }
---
+++ app/src/main/java/com/guilhermefgl/rolling/view/trip/TripFragment.java
@@ -0,0 +1,42 @@
+package com.guilhermefgl.rolling.view.trip;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.guilhermefgl.rolling.R;
+import com.guilhermefgl.rolling.databinding.FragmentTripBinding;
+
+public class TripFragment extends Fragment {
+
+    FragmentTripBinding mBinding;
+
+    public TripFragment() { }
+
+    public static TripFragment newInstance() {
+        return new TripFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_trip, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
