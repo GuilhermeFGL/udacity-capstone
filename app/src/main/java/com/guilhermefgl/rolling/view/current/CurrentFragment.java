35a777fd2ae277cc8a45976f5a9656fd4bc94254
Guilherme Faria <guilherme_fgl@hotmail.com>
2018-05-30 14:52:17 -0300

    add layout current trip

---
+++ app/src/main/java/com/guilhermefgl/rolling/view/current/CurrentFragment.java
@@ -0,0 +1,42 @@
+package com.guilhermefgl.rolling.view.current;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.guilhermefgl.rolling.R;
+import com.guilhermefgl.rolling.databinding.FragmentCurrentBinding;
+
+public class CurrentFragment extends Fragment {
+
+    private FragmentCurrentBinding mBinding;
+
+    public CurrentFragment() { }
+
+    public static CurrentFragment newInstance() {
+        return new CurrentFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_current, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
