dfba5ad3e832edac652975e21b1c79c9eb6f2ae5
Guilherme Faria <guilherme_fgl@hotmail.com>
2018-06-19 11:42:52 -0300

    pre load map

--- app/src/main/java/com/guilhermefgl/rolling/view/splash/SplashActivity.java
+++ app/src/main/java/com/guilhermefgl/rolling/view/splash/SplashActivity.java
@@ -1,16 +1,31 @@
 package com.guilhermefgl.rolling.view.splash;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.guilhermefgl.rolling.R;
 import com.guilhermefgl.rolling.view.BaseActivity;
 import com.guilhermefgl.rolling.view.main.MainActivity;
 
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends BaseActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
+        // pre load map
+        findViewById(R.id.splash_map).setVisibility(View.GONE);
+        ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.splash_map))
+                .getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
         MainActivity.startActivity(this);
     }
 }
