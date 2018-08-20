5a8cbd35b42e6e0cc21060e1afab82ea04c5b01a
Guilherme Faria <guilherme_fgl@hotmail.com>
2018-08-19 21:03:39 -0300

    show marked trips

--- app/src/main/java/com/guilhermefgl/rolling/presenter/list/TripListPresenter.java
+++ app/src/main/java/com/guilhermefgl/rolling/presenter/list/TripListPresenter.java
@@ -1,6 +1,7 @@
 package com.guilhermefgl.rolling.presenter.list;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -12,61 +13,113 @@ import com.guilhermefgl.rolling.model.Trip;
 import com.guilhermefgl.rolling.view.list.TripListViewContract;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TripListPresenter implements TripListPresenterContract, ValueEventListener {
 
     @NonNull
-    private final DatabaseReference mDataBase;
+    private final DatabaseReference mTripDataBase;
     @NonNull
     private final FirebaseAuth mAuth;
     @NonNull
     private final TripListViewContract mView;
+    @Nullable
+    private DatabaseReference mUserHasTripDataBase;
 
+    private List<Trip> mTrips;
+    private List<String> mTripsIds;
     private Filters mFilter;
+    private final ValueEventListener mUserHasTripEventListener;
 
     public TripListPresenter(@NonNull TripListViewContract view) {
-        mDataBase = FirebaseHelper.getTripDatabaseInstance();
+        mTripDataBase = FirebaseHelper.getTripDatabaseInstance();
         mAuth = FirebaseHelper.getAuthInstance();
         mView = view;
+        mUserHasTripEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String currentUser = mAuth.getUid();
+                List<String> tripIds = new ArrayList<>();
+
+                for (DataSnapshot tripSnapshot: dataSnapshot.getChildren()) {
+                    String tripKey = tripSnapshot.getKey();
+                    for (DataSnapshot userSnapshot : tripSnapshot.getChildren()) {
+                        String userKey = userSnapshot.getKey();
+                        if (userKey != null && userKey.equals(currentUser)) {
+                            tripIds.add(tripKey);
+                            break;
+                        }
+                    }
+                }
+                mTripsIds = tripIds;
+                setTrip();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                mView.onDatabaseErrorListener();
+            }
+        };
+
         mView.setPresenter(this);
     }
 
     @Override
     public void setFilter(Filters filter) {
         mFilter = filter;
+        if (mFilter == Filters.MARKED) {
+            mUserHasTripDataBase = FirebaseHelper.getUserDatabaseInstance();
+            mUserHasTripDataBase.addValueEventListener(mUserHasTripEventListener);
+        }
     }
 
     @Override
     public void start() {
-        mDataBase.addValueEventListener(this);
+        mTripDataBase.addValueEventListener(this);
+        if (mUserHasTripDataBase != null)  {
+            mUserHasTripDataBase.addValueEventListener(mUserHasTripEventListener);
+        }
     }
 
     @Override
     public void stop() {
-        mDataBase.removeEventListener(this);
+        mTripDataBase.removeEventListener(this);
+        if (mUserHasTripDataBase != null)  {
+            mUserHasTripDataBase.removeEventListener(mUserHasTripEventListener);
+        }
     }
 
     @Override
     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
         ArrayList<Trip> trips = new ArrayList<>();
-        int orderIndex = 0;
-
-        if (mFilter == Filters.ALL) {
-            trips.add(null);
-            orderIndex = 1;
-        }
-
         for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-            if (mFilter == Filters.ALL) {
-                trips.add(orderIndex, postSnapshot.getValue(Trip.class));
-            }
+            trips.add(postSnapshot.getValue(Trip.class));
         }
-
-        mView.setList(trips);
+        mTrips = trips;
+        setTrip();
     }
 
     @Override
     public void onCancelled(@NonNull DatabaseError databaseError) {
         mView.onDatabaseErrorListener();
+    }
+
+    private void setTrip() {
+        if (mTrips != null) {
+            List<Trip> trips;
+            if (mFilter == Filters.ALL) {
+                trips = new ArrayList<>(mTrips);
+                trips.add(0, null);
+                mView.setList(trips);
+            } else if (mFilter == Filters.MARKED && mTripsIds != null && !mTripsIds.isEmpty()) {
+                trips = new ArrayList<>();
+                for (Trip trip : mTrips) {
+                    if (mTripsIds.contains(trip.getTripId())) {
+                        trips.add(trip);
+                    }
+                }
+                mView.setList(trips);
+            }
+        }
     }
 }
